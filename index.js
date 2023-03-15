90621117d4384529bd87df68e0f30b3ddb368e8b
TZARD69 <zair.hamza69@gmail.com>
2023-03-15 15:40:11 +0100

    netoyage de mon dosier

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-function validateEmail(email) {
-    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    return re.test(email);
-    }
-    
-    function Verification() {
-    // Récupérer lavaleur des champs nom et email
-    var Nom = document.getElementById('name').value;
-    var Email = document.getElementById('email').value;
-    
+
+const form = document.querySelector("#form");
+
+// Récupérer lavaleur des champs nom et email
+   const nom = document.getElementById('name');
+   const email = document.getElementById('email');
+
+form.onsubmit = function (event) {
+
+  event.preventDefault();
+
     // Contrôle sur le nom
-    if(Nom==''){
-    alert('Vous devez compléter votre nom !');
-    document.getElementById('name').style.backgroundColor="red";
-    document.getElementById('name').style.color="#FFF";
-    
-    // Permet de bloquer l'envoi du formulaire
-    return false;
+    if (nom.value === '') {
+        
+        document.getElementById('name').style.backgroundColor = "red";
+        document.getElementById('name').style.color = "#FFF";
+
+        return alert('Vous devez compléter votre nom !');
+        // Permet de bloquer l'envoi du formulaire
+       
     }
-    else{
-    document.getElementById('name').style.backgroundColor="#9C6";
+    else {
+        document.getElementById('name').style.backgroundColor = "#9C6";
     }
-    
+
     // Contrôle sur l'email
-    if(Email=='') {
-    alert('Vous devez compléter votre adresse email');
-    document.getElementById('email').style.backgroundColor="red";
-    document.getElementById('email').style.color="#FFF";
-    return false;
+    if (email.value === '') {
+        
+        document.getElementById('email').style.backgroundColor = "red";
+        document.getElementById('email').style.color = "#FFF";
+      return alert('Vous devez compléter votre adresse email');
     }
-    
-    else{
-    document.getElementById('email').style.backgroundColor="#9C6";
+
+    else {
+        document.getElementById('email').style.backgroundColor = "#9C6";
     }
-    }
-    
+}
