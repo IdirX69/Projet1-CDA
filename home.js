ca07344ba31399d93bc775a3a40244f3b27e431b
IdirX69 <adil.idirr@gmail.com>
2023-03-16 16:54:26 +0100

    maj home soft skills

--- home.js
+++ home.js
@@ -1,18 +1,55 @@
 const navBar = document.querySelector(".navigation");
 const target = document.getElementById("text-target");
 let array = [
-  "React",
-  "Javascript",
-  "Html",
-  "Css",
-  "Node Js",
-  "Mysql",
-  "Express",
-  "React Native",
-  "Graph QL",
+  "Communication",
+  "Collaboration",
+  "Créativité",
+  "Adaptabilité",
+  "Autonomie",
+  "Esprit d'équipe",
+  "Empathie",
+  "Leadership",
+  "Curiosité",
+  "Persévérance",
+  "Organisation",
 ];
 let wordIndex = 0;
 let letterIndex = 0;
+
+const createLetter = () => {
+  const letter = document.createElement("span");
+  target.appendChild(letter);
+  letter.style.opacity = "0";
+  letter.style.animation = "anim 4.5s ease forwards";
+  letter.textContent = array[wordIndex][letterIndex];
+  setTimeout(() => {
+    letter.remove();
+  }, 2000);
+};
+
+setTimeout(() => {
+  const loop = () => {
+    setTimeout(() => {
+      if (wordIndex >= array.length) {
+        wordIndex = 0;
+        letterIndex = 0;
+        loop();
+      } else if (letterIndex < array[wordIndex].length) {
+        createLetter();
+        letterIndex++;
+        loop();
+      } else {
+        letterIndex = 0;
+        wordIndex++;
+        setTimeout(() => {
+          loop();
+        }, 2000);
+      }
+    }, 40);
+  };
+  createLetter();
+  loop();
+}, 1500);
 
 // mouse effect
 
@@ -83,40 +120,6 @@ setTimeout(() => {
 }, 2800);
 setTimeout(() => {
   titleLoop(iAmWild, devText, devtextIndex);
-}, 1500);
-
-const createLetter = () => {
-  const letter = document.createElement("span");
-  target.appendChild(letter);
-  letter.style.opacity = "0";
-  letter.style.animation = "anim 5s ease forwards";
-  letter.textContent = array[wordIndex][letterIndex];
-  setTimeout(() => {
-    letter.remove();
-  }, 2000);
-};
-setTimeout(() => {
-  const loop = () => {
-    setTimeout(() => {
-      if (wordIndex >= array.length) {
-        wordIndex = 0;
-        letterIndex = 0;
-        loop();
-      } else if (letterIndex < array[wordIndex].length) {
-        createLetter();
-        letterIndex++;
-        loop();
-      } else {
-        letterIndex = 0;
-        wordIndex++;
-        setTimeout(() => {
-          loop();
-        }, 2100);
-      }
-    }, 60);
-  };
-  createLetter();
-  loop();
 }, 1500);
 
 const icons = document.querySelectorAll(".social-network .fa-brands");
