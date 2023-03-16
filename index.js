6508d70db680c211aafdd1bb7f9149429b8b05f4
TZARD69 <zair.hamza69@gmail.com>
2023-03-16 10:47:15 +0100

    fichier js fini

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 
 const form = document.querySelector("#form");
 
-// Récupérer lavaleur des champs nom et email
-   const nom = document.getElementById('name');
-   const email = document.getElementById('email');
+// Récupérer la valeur des champs nom et email
+const nom = document.querySelector('#name');
+const email = document.querySelector('#email');
+const Messg = document.querySelector('#msg')
 
 form.onsubmit = function (event) {
 
   event.preventDefault();
 
-    // Contrôle sur le nom
-    if (nom.value === '') {
-        
-        document.getElementById('name').style.backgroundColor = "red";
-        document.getElementById('name').style.color = "#FFF";
+  // Contrôle sur le nom
+  if (nom.value === '') {
 
-        return alert('Vous devez compléter votre nom !');
-        // Permet de bloquer l'envoi du formulaire
-       
-    }
-    else {
-        document.getElementById('name').style.backgroundColor = "#9C6";
-    }
+    alert('Vous devez compléter votre nom !');
+    nom.style.backgroundColor = "#a50f0f";
 
-    // Contrôle sur l'email
-    if (email.value === '') {
-        
-        document.getElementById('email').style.backgroundColor = "red";
-        document.getElementById('email').style.color = "#FFF";
-      return alert('Vous devez compléter votre adresse email');
-    }
+  }
+  else {
+    nom.style.backgroundColor = "#096024";
+  }
 
-    else {
-        document.getElementById('email').style.backgroundColor = "#9C6";
-    }
+  // Contrôle sur l'email
+  if (email.value === '') {
+
+    alert('Vous devez compléter votre adresse email');
+
+    email.style.backgroundColor = "#a50f0f";
+    return false
+  }
+
+  else {
+    email.style.backgroundColor = "#096024";
+  }
+  form.addEventListener('submit', () => {
+    alert('Merci')
+    email.value = ''
+    email.style.backgroundColor = ''
+    nom.value = ''
+    nom.style.backgroundColor = ''
+    Messg.value = ''
+  })
 }
