1e1e5e2077fa59231ca60b795b301b1e6da48680
Sahrane_Guassemi <sahranedev@gmail.com>
2023-03-20 18:12:25 +0100

    addanimeJs skills + rebase hmtl form and css + js application of contact section

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const goToTop = () => {
 
 backToTopButton.addEventListener("click", goToTop);
 
-const form = document.querySelector("#form");
+/* const form = document.querySelector("#form");
 
 // Récupérer la valeur des champs nom et email
 const nom = document.querySelector("#name");
@@ -31,7 +31,6 @@ form.onsubmit = function (event) {
 
   // Contrôle sur le nom
   if (nom.value === "") {
-    alert("Vous devez compléter votre nom !");
     nom.style.backgroundColor = "#a50f0f";
   } else {
     nom.style.backgroundColor = "#096024";
@@ -39,15 +38,12 @@ form.onsubmit = function (event) {
 
   // Contrôle sur l'email
   if (email.value === "") {
-    alert("Vous devez compléter votre adresse email");
-
     email.style.backgroundColor = "#a50f0f";
     return false;
   } else {
     email.style.backgroundColor = "#096024";
   }
   form.addEventListener("submit", () => {
-    alert("Merci");
     email.value = "";
     email.style.backgroundColor = "";
     nom.value = "";
@@ -55,3 +51,39 @@ form.onsubmit = function (event) {
     Messg.value = "";
   });
 };
+ */
+const submitButton = document.querySelector(".submit-button");
+const messageEmpty = document.querySelector("#message-error");
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const emailValue = document.querySelector(".email-input").value;
+  const emailInput = document.querySelector(".email-input");
+  const nameValue = document.querySelector(".name-input").value;
+  const nameInput = document.querySelector(".name-input");
+  const messageValue = document.querySelector(".user-message").value;
+  const messageInput = document.querySelector(".user-message");
+
+  switch (true) {
+    case emailValue === "" && nameValue === "" && messageValue === "":
+      emailInput.style.backgroundColor = "#ea1212";
+      nameInput.style.backgroundColor = "#ea1212";
+      messageInput.style.backgroundColor = "#ea1212";
+
+      messageEmpty.innerHTML = "Veuillez remplir tous les champs";
+      break;
+    case nameValue === "":
+      messageEmpty.innerHTML = "Veuillez renseigner votre nom";
+      break;
+    case emailValue === "":
+      messageEmpty.innerHTML = "Veuillez renseigner une adresse email";
+      break;
+    case messageValue === "":
+      messageInput.style.backgroundColor = "#ea1212";
+      messageEmpty.innerHTML = "Veuillez insérer un message";
+      break;
+    case (messageInput.style.backgroundColor = "#ea1212" && !emailValue):
+      messageInput.style.backgroundColor = "";
+      break;
+  }
+});
